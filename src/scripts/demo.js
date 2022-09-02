e66b79d668039821000f5cb75d419d66dbdab1bb
tqb310 <baotq310@gmail.com>
2022-09-02 09:00:25 +0700

    commit

--- src/scripts/demo.js
+++ src/scripts/demo.js
@@ -1,1 +1,39 @@
-console.log('this is a de')
+let canvas = document.querySelector('#draw');
+let arr = document.querySelectorAll('.container__child-item');
+console.log(canvas.getBoundingClientRect())
+
+let isMouseUp = true;
+let selectColor = 'red';
+let colors = ['red','yellow','orange','green','blue','indigo','purple']
+canvas.addEventListener('mousemove', (e) => {
+    if (isMouseUp)
+        return;
+    //console.log(e.clientX);
+    let { top, left } = canvas.getBoundingClientRect();
+    render(e.clientX - left - 10, e.clientY - top + 35, selectColor);
+})
+canvas.addEventListener('mouseup', (e) => {
+    isMouseUp = true;
+})
+canvas.addEventListener('mousedown', (e) => {
+    isMouseUp = false;
+})
+
+function render(x, y, color) {
+    let ctx = canvas.getContext('2d');
+    ctx.fillStyle = color;
+    let circle = new Path2D();
+    circle.arc(x, y, 5, 0, 2 * Math.PI);
+    ctx.fill(circle);
+}
+
+function abc(){
+    arr.forEach((item, index) => {
+        item.addEventListener('click', (e) => {
+            console.log(colors[index]);
+            selectColor = colors[index];
+        })
+    })
+}
+
+abc();
